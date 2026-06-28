fdab2ceac8cb1d43205e34fefa27accd28607621
cak <cak@typeerror.com>
2026-06-28 08:32:01 -0400

    fix: update knip configuration to correct entry file and enhance ignoreDependencies

--- knip.ts
+++ knip.ts
@@ -1,14 +1,17 @@
 import type { RawConfigurationOrFn } from "knip/dist/types/config.js";
 
 const config: RawConfigurationOrFn = {
+  ignoreIssues: {
+    "pnpm-workspace.yaml": ["catalog"],
+  },
   workspaces: {
     ".": {
-      entry: ["caido.config.ts", "eslint.config.mjs"],
+      entry: ["caido.config.ts", "vite.config.ts"],
     },
     "packages/backend": {
       entry: ["src/index.ts"],
       project: ["src/**/*.ts"],
-      ignoreDependencies: ["caido"],
+      ignoreDependencies: ["caido", "sqlite"],
     },
     "packages/frontend": {
       entry: ["src/index.ts"],
